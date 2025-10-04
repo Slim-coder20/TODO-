0f4a77d5082fc7c87ea85d4fad8ee9ee3935d872
slim <slimabida21@gmail.com>
2025-10-04 22:34:09 +0200

    Mise en place de fonction addTask pour l'ajout d'une nouvelle tache

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -3,13 +3,30 @@ import Header from "./header/Header";
 import TaskInput from "./taskinput/TaskInput";
 import TaskList from "./taskList/TaskList";
 import Footer from "./footer/Footer";
-
+import { useState } from "react";
 // Ce composant va nous permettre d'affiché l'integralité des composants de notre todo  //
 export default function TaskContainer() {
+  /*State : ce State va ête l'état de la liste des tâches qui va nous servir 
+  dans toutes les opérations c'est à dire modifier ou supprimer ou ajouter une Tâche .
+  */
+  const [tasksList, setTasksList] = useState([]);
+  console.log(tasksList); 
+
+  // Function
+  /* Cette fonction nous servira a ajouter une tâche */
+  const addTask = (title) => {
+    const newTask = {
+      id: tasksList.length + 1,
+      title: title,
+      completed: false,
+    };
+    setTasksList([...tasksList, newTask]);
+  };
+
   return (
     <main>
       <Header />
-      <TaskInput />
+      <TaskInput addTask={addTask} />
       <TaskList />
       <Footer />
     </main>
--- src/components/taskinput/TaskInput.jsx
+++ src/components/taskinput/TaskInput.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import styles from "./taskInput.module.css";
+import { useState } from "react";
 
-export default function TaskInput() {
+// Ce composant va nous permettre de créer des tâches
+export default function TaskInput({ addTask }) {
+  // State //
+  const [taskTitle, setTaskTitle] = useState("");
+
+  // On commence par créer une fonction qui va nous permettre de récupérer ce qui est saisie dans l'input //
+  // Function
+  const handleInputChange = (event) => {
+    //event.preventDefault();
+    setTaskTitle(event.target.value);
+  };
+
+  // cette fonction va nous permettre de soumettre l'ajout d'une nvelle Tâche //
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    if (taskTitle.trim()) {
+      addTask(taskTitle);
+      setTaskTitle("");
+    }
+  };
+
   return (
     <div className={`box ${styles.element}`}>
       <h2 className={styles.title}>Ajoute ta prochaine Tache</h2>
-      <form className={styles.container}>
+      <form className={styles.container} onSubmit={handleAddTask}>
         <input
+          onChange={handleInputChange}
           type="text"
           className={styles.input}
           placeholder=" Indiquez un titre à votre tâche. "
+          value={taskTitle}
         />
         <button className="button-primary" type="submit">
           Ajouter
