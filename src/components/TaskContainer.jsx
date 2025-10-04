65aa6c6adb2dd32911d8ae26c02ddd03285400a6
slim <slimabida21@gmail.com>
2025-10-04 14:47:46 +0200

    Mise en place de l'architecture du projet avec la création des composant

---
+++ src/components/TaskContainer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+// Ce composant va nous permettre d'affiché l'integralité de nos taches //
+export default function TaskContainer() {
+  return <main>Notre contenu ici ...</main>;
+}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+
+import React from 'react'
+import styles from './Header.module.css'
+import reactLogo from '../../assets/react.svg'
+
+export default function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.titleContainer}>
+        <img src={reactLogo} alt="Logo" width={50} height={50}/>
+        <div>
+          <h1>Todo</h1>
+          <div className="color-gray">
+            <code>Éliminez le chaos, suivez le flux.</code>
+          </div>
+        </div>
+
+      </div>
+      <code className="color-primary"> 
+        V.1.0 
+      </code>
+      
+    </div>
+  )
+}
