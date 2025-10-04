54117e7929df885676c591b948135f9da6bb14ad
slim <slimabida21@gmail.com>
2025-10-04 17:33:27 +0200

    import du composant TaskList.jsx

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "./header/Header";
 import TaskInput from "./taskinput/TaskInput";
+import TaskList from "./taskList/TaskList";
 
-// Ce composant va nous permettre d'affiché l'integralité de nos taches //
+// Ce composant va nous permettre d'affiché l'integralité des composants de notre todo  //
 export default function TaskContainer() {
   return (
     <main>
       <Header />
       <TaskInput />
+      <TaskList />
     </main>
   );
 }
